115f30a7ea6bfde2640a447144e04c5809c504c4
MustBmaster <minhmocprovdqq@gmail.com>
2024-08-30 00:03:47 +0700

    #2
    temp

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -5,25 +5,30 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 public class UserController {
 
     private final UserService userService;
-    
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser(){
-        User user = new User();
-        user.setUsername("minh");
-        user.setPassword("minhminh2k");
-        user.setEmail("minhnv1@viettel.com");
-        this.userService.setNewUser(user);
-        return "User created successfully";
+    // @GetMapping("/user/create")
+    @PostMapping("/user")
+    public User createNewUser(@RequestBody User RqUser){
+        User newUser= this.userService.setNewUser(RqUser);
+        return newUser;
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestBody User RqUser){
+        this.userService.deleteUser(RqUser);
     }
 }
